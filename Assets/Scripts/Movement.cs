5a28646dea82399e8eb5f64bf0a766965d85c6df
rjd_gamer <jman248@comcast.net>
2022-05-08 15:14:14 -0700

    L/R Movement Works

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,7 +7,12 @@ public class Movement : MonoBehaviour
 	
 	public bool isGrounded = false;
 	public float thrust = 1.0f; 
-
+	
+	//speed vars
+	public float groundSpeed = .2f; 
+	public float airSpeed = .1f; 
+	
+	public float speed = 1.0f;
 	private Rigidbody2D rb2D;	
 	
 	
@@ -19,21 +24,34 @@ public class Movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-		
-		
+		if(!this.isGrounded){
+			if(this.speed > this.airSpeed){
+				this.speed -= .002f; 
+				
+			}
+			
+		}
+		//this.speed = (this.isGrounded) ? this.groundSpeed : this.airSpeed; 
         //Static Jumping
 		//On Space Bar Pressed
-		if(Input.GetKeyDown(KeyCode.Space)){
+		if(Input.GetAxisRaw("Jump") > 0){
 			//If isGrounded
 			if(isGrounded){
-				rb2D.AddForce(transform.up * thrust);
+				isGrounded = false; 
+				rb2D.AddForce(transform.up * thrust, ForceMode2D.Impulse);
 			}
 		}
+	
 		
-				
-		//Left Movement
+			
+
+
+			
+		//Move left & right
+		transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * speed, 0, 0)); 
+
 		
 		//Right Movement
     }
@@ -47,8 +65,10 @@ public class Movement : MonoBehaviour
 	}
 	//On Collision Enter
 	void OnCollisionEnter2D(Collision2D c) {
+		
         //If Collision Tag is Ground
-		if(c.gameObject.tag == "ground"){
+		if(c.gameObject.CompareTag("ground")){
+			this.speed = this.groundSpeed; 
 			//isGrounded = True 
 			isGrounded = true; 
 		}
