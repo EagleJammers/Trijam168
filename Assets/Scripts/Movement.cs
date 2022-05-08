ef97fcaa4a1858445f0dd08cd5a8d3eb9ad21cfa
rjd_gamer <jman248@comcast.net>
2022-05-08 14:07:51 -0700

    Player Jump Pseudocode

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	
+	private bool isGrounded; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+		
+        //Static Jumping
+		//On Space Bar Pressed
+			//If isGrounded
+				//Do Jump Command 
+		
+		
+				
+		//Left Movement
+		
+		//Right Movement
+    }
+	
+	//On Collision Exit
+		//If Collision Tag is Ground
+			//isGrounded = false
+			
+	//On Collision Enter
+		//If Collision Tag is Ground	
+			//isGrounded = True 
+
+}
